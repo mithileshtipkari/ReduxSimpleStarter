ba2074fb315340d5336a3699fcd345a01e56cd04
mithilesht <mithilesht@cdac.in>
2019-10-09 18:05:02 +0530

    Changed index to render search box

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar.js';
 
 const App = () => {
-  return <div>ok im here</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
