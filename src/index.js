38409b0436771a4f30b24729feb6eda176baf0af
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-10-16 00:45:04 +0530

    Searching videos using Youtube API

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import ok from './components/video_details';
+import YTSearch from 'youtube-api-search';
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+const API_KEY = 'AIzaSyATa1WV3At4zP1Br0E5Ce81B-MZ1MFeuO0';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos : [] };
+
+    YTSearch({key : API_KEY, term : 'maroon5'}, (videos) => {
+      this.setState({ videos }); // this.setState({ videos : videos});
+    });
+  }
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
