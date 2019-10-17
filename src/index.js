14d137aa8e452e779455d4f49f5dac0dd2488f06
mithileshtipkari <mithilesh.tipkari@gmail.com>
2019-10-17 17:18:41 +0530

    Playing video on select

--- src/index.js
+++ src/index.js
@@ -10,18 +10,24 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = { videos : [] };
+    this.state = {
+      videos : [],
+      selectedVideo : null};
 
     YTSearch({key : API_KEY, term : 'endgame'}, (videos) => {
-      this.setState({ videos }); // this.setState({ videos : videos});
+      this.setState({
+        videos : videos,
+        selectedVideo : videos[0]}); // this.setState({ videos : videos});
     });
   }
   render(){
     return (
       <div>
         <SearchBar />
-        <VideoDetail video={this.state.videos[0]}/>
-        <VideoList videos={this.state.videos}/>
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })} 
+          videos={this.state.videos}/>
       </div>
     );
   }
